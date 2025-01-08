7e5fc667a294b2aea8242b30be422735d0fe8ad0
Muskan96312 <muskankumari9631767@gmail.com>
2025-01-08 15:12:49 +0530

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -1,17 +1,22 @@
-const btn = document.getElementById("btn");
-const container = document.getElementById("container");
+document.addEventListener('DOMContentLoaded', function () {
+    const button = document.getElementById('displayButton');
+    const output = document.getElementById('output');
+    
+//     button.addEventListener('click', function () {
+  document.getElementById("displayButton").addEventListener("click", function() {
+    console.log("Button clicked!");
+      fetch('https://jsonplaceholder.typicode.com/posts/1')  
+         .then(response => response.json())  
+         .then(data => {
+    
+          document.getElementById('userId').textContent = data.userId;
+          document.getElementById('title').textContent = data.title;
+          document.getElementById('body').textContent = data.body;
+          document.getElementById('ID').textContent = data.id;
+          
+          document.getElementById("output").style.display = 'block';
+        })
+        .catch(error => console.error('Error:', error));
+    });
+});
 
-async function clickHandler() {
-	const data = await fetch("https://jsonplaceholder.typicode.com/posts/1")
-	const dataToJson = await data.json()
-	// console.log(dataToJson)
-	container.innerHTML = JSON.stringify(dataToJson)
-}
-
-function mouseOutHandler() {
-	container.innerHTML = "";
-}
-
-btn.addEventListener("click", clickHandler);
-// btn.addEventListener("mouseover", clickHandler);
-// btn.addEventListener("mouseout", mouseOutHandler);
